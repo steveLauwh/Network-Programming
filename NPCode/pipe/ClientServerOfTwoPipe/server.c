8bae644e79918fc41ff83d19241fdb1bbdfd5fe4
Steve Lau <liuwenhong_steve@163.com>
2017-12-27 10:47:22 +0800

    服务端

---
+++ NPCode/pipe/ClientServerOfTwoPipe/server.c
@@ -0,0 +1,37 @@
+#include "server.h"
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>  // O_RDONLY
+#include <errno.h>  // errno
+#include <string.h> // strerror
+
+#define MAXLINE 4096
+
+void server(int readfd, int writefd)
+{
+	int fd;
+	ssize_t n;
+
+	char buf[MAXLINE+1];
+	
+	// 从管道1读出
+	if ((n = read(readfd, buf, MAXLINE)) < 0) {
+		perror("server read");
+		exit(EXIT_FAILURE);
+	}
+	buf[n] = '\0';
+
+	if ((fd = open(buf, O_RDONLY)) < 0) {
+		snprintf(buf + n, sizeof(buf) - n, ": can't open, %s\n", strerror(errno));
+		n = strlen(buf);
+		write(writefd, buf, n);
+	} else {
+		while ((n = read(fd, buf, MAXLINE)) > 0) {
+			write(writefd, buf, n);  // 写入管道2
+		}
+
+		close(fd);
+	}
+}
+
