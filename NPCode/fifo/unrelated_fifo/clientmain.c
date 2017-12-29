06d10f5216300d41ab1ac368c2c11b77e76cbdc6
Steve Lau <liuwenhong_steve@163.com>
2017-12-29 16:28:42 +0800

    Create clientmain.c

---
+++ NPCode/fifo/unrelated_fifo/clientmain.c
@@ -0,0 +1,35 @@
+#include "client.h"
+#include "fifo.h"
+
+#define ERR_EXIT(x) \
+	do {		\
+		perror(x); \
+		exit(EXIT_FAILURE); \
+	} while(0)
+
+int main(int argc, char *argv[])
+{
+	int readfd, writefd;
+
+	// 调用 open，作为客户端，注意必须先写后读，否则会造成死锁
+	if ((writefd = open(FIFO1, O_WRONLY, 0)) < 0) {
+		ERR_EXIT("open FIFO1");
+	}
+
+	if ((readfd = open(FIFO2, O_RDONLY, 0)) < 0) {
+		ERR_EXIT("open FIFO2");
+	}
+
+	client(readfd, writefd);
+
+	close(readfd);
+
+	close(writefd);
+
+	unlink(FIFO1); // 使用 unlink 将 FIFO 名字从文件系统中删除
+	
+	unlink(FIFO2);
+
+	exit(EXIT_SUCCESS);
+}
+
